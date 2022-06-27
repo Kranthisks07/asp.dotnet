487e0ebdbedd3b6b261e3c7760c1c2413869c881
Kranthisks07 <104075380+Kranthisks07@users.noreply.github.com>
2022-06-27 18:31:03 +0530

    Add files via upload

---
+++ Codebase_5/Data/Codebase_5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Codebase_5.Models;
+
+namespace Codebase_5.Data
+{
+    public class Codebase_5Context : DbContext
+    {
+        public Codebase_5Context (DbContextOptions<Codebase_5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Codebase_5.Models.Student> Student { get; set; }
+    }
+}
---
+++ Codebase_5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Codebase_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
